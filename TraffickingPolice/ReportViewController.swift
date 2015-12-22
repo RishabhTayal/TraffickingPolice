82592d8b17fa4d55fd4b5078d3c91adecc6d44e4
Rishabh Tayal <rtayal11@gmail.com>
2015-12-21 22:00:50 -0600

    added report form

--- TraffickingPolice/ReportViewController.swift
+++ TraffickingPolice/ReportViewController.swift
@@ -7,18 +7,108 @@
 //
 
 import UIKit
+import XLForm
 
-class ReportViewController: UIViewController {
+class ReportViewController: XLFormViewController {
 
+    private struct Tags {
+        static let Name = "name"
+        static let Email = "email"
+        static let Twitter = "twitter"
+        static let Number = "number"
+        static let Integer = "integer"
+        static let PickerView = "selectorPickerView"
+        static let Decimal = "decimal"
+        static let Password = "password"
+        static let Image = "image"
+        static let Phone = "phone"
+        static let Url = "url"
+        static let ZipCode = "zipCode"
+        static let TextView = "textView"
+        static let Notes = "notes"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialForm()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Report"
     }
+    
+    func initialForm() {
+        
+        let form : XLFormDescriptor
+        var section : XLFormSectionDescriptor
+        var row : XLFormRowDescriptor
+        
+        form = XLFormDescriptor(title: "Text Fields")
+        form.assignFirstResponderOnShow = true
+        
+        section = XLFormSectionDescriptor.formSectionWithTitle("TextField Types")
+        section.footerTitle = "This is a long text that will appear on section footer"
+        form.addFormSection(section)
+        
+        row = XLFormRowDescriptor(tag: Tags.Image, rowType: XLFormRowDescriptorTypeImage, title: "Image")
+        row.value = UIImage(named: "default_avatar")
+        section.addFormRow(row)
+        
+        // Name
+        row = XLFormRowDescriptor(tag: Tags.Name, rowType: XLFormRowDescriptorTypeText, title: "Name")
+        row.required = true
+        section.addFormRow(row)
+        
+        // Age
+        row = XLFormRowDescriptor(tag: Tags.PickerView, rowType:XLFormRowDescriptorTypeSelectorPickerView, title:"Age")
+        row.selectorOptions = ["0-10", "11-18", "19-25", "26-35", "36-50"]
+//        row.value = "11-18"
+        section.addFormRow(row)
+        
+        // Zip Code
+        row = XLFormRowDescriptor(tag: Tags.ZipCode, rowType: XLFormRowDescriptorTypeZipCode, title: "Zip Code")
+        section.addFormRow(row)
+        
+//        // Number
+//        row = XLFormRowDescriptor(tag: Tags.Number, rowType: XLFormRowDescriptorTypeNumber, title: "Number")
+//        section.addFormRow(row)
+        
+        // Integer
+        row = XLFormRowDescriptor(tag: Tags.Integer, rowType: XLFormRowDescriptorTypeInteger, title: "Integer")
+        section.addFormRow(row)
+        
+        // Phone
+        row = XLFormRowDescriptor(tag: Tags.Phone, rowType: XLFormRowDescriptorTypePhone, title: "Phone")
+        section.addFormRow(row)
+        
+        // Url
+        row = XLFormRowDescriptor(tag: Tags.Url, rowType: XLFormRowDescriptorTypeURL, title: "Url")
+        section.addFormRow(row)
+        
+        section = XLFormSectionDescriptor.formSection()
+        form.addFormSection(section)
+        
+        // TextView
+        row = XLFormRowDescriptor(tag: Tags.TextView, rowType: XLFormRowDescriptorTypeTextView)
+        row.cellConfigAtConfigure["textView.placeholder"] = "TEXT VIEW EXAMPLE"
+        section.addFormRow(row)
+        
+        section = XLFormSectionDescriptor.formSectionWithTitle("TextView With Label Example")
+        form.addFormSection(section)
+        
+        self.form = form
+    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func formRowDescriptorValueHasChanged(formRow: XLFormRowDescriptor!, oldValue: AnyObject!, newValue: AnyObject!) {
+        super.formRowDescriptorValueHasChanged(formRow, oldValue: oldValue, newValue: newValue)
+        if formRow.tag == Tags.Image {
+            let newRow = formRow.copy() as! XLFormRowDescriptor
+            newRow.tag = "secondAlert"
+//            newRow.title = "Second Alert"
+            form.addFormRow(newRow, afterRow:formRow)
+        }
     }
 }
 
