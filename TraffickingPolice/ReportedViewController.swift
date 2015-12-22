d0da12790cf2aa1b3875bdbde21dce3366927d5b
Rishabh Tayal <rtayal11@gmail.com>
2015-12-22 08:57:39 -0600

    reported list

--- TraffickingPolice/ReportedViewController.swift
+++ TraffickingPolice/ReportedViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import Parse
 
-class ReportedViewController: UIViewController {
+class ReportedViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView: UITableView!
+    var datasourceArray: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Reported"
+        
+        tableView = UITableView(frame: self.view.frame, style: .Grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+        
+        let query = PFQuery(className: "Reported")
+        query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) -> Void in
+            if let objects = objects {
+                self.datasourceArray = objects
+                self.tableView.reloadData()
+            }
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: UITableView Datasource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasourceArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("cell")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        }
+        let object = datasourceArray[indexPath.row]
+        cell?.textLabel?.text = object["name"] as? String
+        return cell!
     }
 }
