022a2465b27db53a3b24e84897cfb124ccbbb14c
Rishabh Tayal <rtayal11@gmail.com>
2015-12-23 18:42:59 -0600

    added pulsing halo added tab icons

--- MainViewController.swift
+++ MainViewController.swift
@@ -7,20 +7,27 @@
 //
 
 import UIKit
+import PulsingHalo
 
 class MainViewController: UIViewController {
     
     @IBOutlet var reportButton: UIButton!
+    var halo: PulsingHaloLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         reportButton.roundView()
+        
+        halo = PulsingHaloLayer()
+        halo.radius = reportButton.frame.size.width + 60
+        halo.backgroundColor = reportButton.backgroundColor?.CGColor
+        reportButton.superview?.layer.insertSublayer(halo, below: reportButton.layer)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        halo.position = reportButton.center
     }
     
     @IBAction func buttonTapped(sender: AnyObject) {
