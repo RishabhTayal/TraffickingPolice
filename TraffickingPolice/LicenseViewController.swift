9abc554de24f00b1e298702342e4eb2914ae431e
Rishabh Tayal <rtayal11@gmail.com>
2016-02-01 12:40:24 -0600

    commit

--- TraffickingPolice/LicenseViewController.swift
+++ TraffickingPolice/LicenseViewController.swift
@@ -31,7 +31,7 @@ class LicenseViewController: UIViewController {
             
         }
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: "cancelTappe:")
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: "cancelTapped:")
     }
     
     func cancelTapped(sender: AnyObject) {
